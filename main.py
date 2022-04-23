d12c84490121e5bba865951f5536a61dd34a8334
FlickZaur <zaurmarem87@mail.ru>
2022-04-24 00:27:08 +0300

    homework Варианты выбора

--- main.py
+++ main.py
@@ -1,51 +1,33 @@
-import flask
 from flask import Flask, url_for
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def zag():
-    return "Миссия Колонизация Марса"
-
-
-@app.route('/index')
-def index():
-    return "И на Марсе будут яблони цвести!"
-
-
-@app.route('/promotion')
-def prom():
-    return '<br>'.join(['Человечество вырастает из детства.', 'Человечеству мала одна планета.',
-                        'Мы сделаем обитаемыми безжизненные пока планеты.', 'И начнем с Марса!', 'Присоединяйся!'])
-
-
-@app.route('/image_mars')
-def image():
-    return f'''<h1>Жди нас, Марс</h1>
-<img src="{url_for('static', filename='img/mars.jpg')}" 
-           alt="здесь должна была быть картинка, но не нашлась">
-<h3>Вот как он выглядит!</h3>'''
-
-
-@app.route('/promotion_image')
-def promotion_image():
-    promo_spisok = ['Человечество вырастает из детства.', 'Человечеству мала одна планета.',
-                    'Мы сделаем обитаемыми безжизненные пока планеты.', 'И начнем с Марса!', 'Присоединяйся!']
-    return f'''<!doctype html>
-                <html lang="en">
-                  <head>
-                    <meta charset="utf-8">
-                    <link rel="stylesheet" type="text/css" href="{url_for('static', filename='css/some.css')}" />
-                    <title>Привет, Яндекс!</title>
-                  </head>
-                    <body>           
-                <h1>Жди нас, Марс!<h1>
-        <img src="{flask.url_for('static', filename='img/mars.jpg')}" alt="здесь должна была быть картинка, 
-        но не нашлась">
-        <h1>Вот она, красная планета!<h1>''' \
-           + '</br>'.join([f"<h1 id=f{index}>{line}</h1>" for index, line in enumerate(promo_spisok)]) + \
-           '</body> </html>'
+@app.route('/choice/<planet_name>')
+def form_sample(planet_name='Земля'):
+        return f'''<!doctype html>
+                        <html lang="en">
+                          <head>
+                            <meta charset="utf-8">
+                            <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+                            <link rel="stylesheet"
+                            href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css"
+                            integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1"
+                            crossorigin="anonymous">
+                            <link rel="stylesheet" type="text/css" href="{url_for('static', filename='css/style.css')}" 
+                            />
+                            <title>про планеты</title>
+                          </head>
+                          <body>
+                          <h1> Я думаю, что  {planet_name} -</h1>
+                          <h3>Это планета:)</h3>
+                          <h2> Она красивая<3 </h2>
+                          <h6>тест на зрение)))</h6>
+                           <link rel="stylesheet" type="text/css" 
+                           href="{url_for('static', filename='css/some.css')}" />
+                                        
+                          </body>
+                        </html>'''
 
 
 if __name__ == '__main__':
